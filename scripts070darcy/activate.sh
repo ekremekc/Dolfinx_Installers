d31bbd5676697e6bf5003c940296cd055e511994
Ekrem Ekici <65715006+ekremekc@users.noreply.github.com>
2023-10-26 15:37:53 +0100

    Add files via upload

---
+++ scripts070darcy/activate.sh
@@ -0,0 +1,32 @@
+export VENV_DIR=/home/ee331/Dev/Venvs/v070complex
+export UTILS_DIR=/home/ee331/Dev/Venvs/dolfinx_utils
+
+source $VENV_DIR/bin/activate 
+
+# sourcing correct gcc for multiphenicsx
+export CC=/home/ee331/bin/gcc
+export CXX=/home/ee331/bin/g++
+export LD_LIBRARY_PATH=$HOME/gcc-12/lib64:$LD_LIBRARY_PATH
+
+export OMPI=$UTILS_DIR/openmpi
+export PATH=$OMPI/bin:$PATH
+export LD_LIBRARY_PATH=$OMPI/lib:$LD_LIBRARY_PATH
+#export OMP_NUM_THREADS=1
+
+export KAHIP=$UTILS_DIR/KaHIP
+export PATH=$KAHIP/bin:$PATH
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$KAHIP/lib
+
+export ADIOS2=$UTILS_DIR/adios2
+export PATH=$ADIOS2/bin:$PATH
+export LD_LIBRARY_PATH=$ADIOS2/lib:$LD_LIBRARY_PATH
+
+
+# Activating complex mode
+export PKG_CONFIG_PATH=$VENV_DIR/installation/dolfinx-complex/lib/pkgconfig:$PKG_CONFIG_PATH
+export PETSC_ARCH=linux-gnu-complex-32 
+export PYTHONPATH=$VENV_DIR/lib/python3.8/dist-packages:/usr/bin/python3:$PYTHONPATH
+export LD_LIBRARY_PATH=$VENV_DIR/installation/lib:$LD_LIBRARY_PATH
+
+# For ADIOS2
+#export PYTHONPATH=$VENV_DIR/installation/adios2/home/ee331/Dev/Venvs/v060/lib/python3.8/site-packages/adios2:$PYTHONPATH
---
+++ scripts070darcy/gcc_install.sh
@@ -0,0 +1,34 @@
+# This script is prepared to install gcc 12.2 from source.
+# Dolfinx uses cpp 20 so this compiler is necessary to build latest versions of dolfinx.
+
+mkdir $HOME/gcc-install
+cd $HOME/gcc-install
+export gcc_ver=12.3.0 
+wget -nc --quiet https://mirrorservice.org/sites/sourceware.org/pub/gcc/releases/gcc-$gcc_ver/gcc-$gcc_ver.tar.gz
+tar -xf gcc-$gcc_ver.tar.gz
+
+export srcdir=$HOME/gcc-install/gcc-$gcc_ver
+export objdir=$HOME/gcc-$gcc_ver
+
+mkdir $objdir && cd $objdir
+
+# configure and build gcc
+$srcdir/configure --prefix=$objdir --disable-nls --enable-languages=c,c++,fortran --without-headers --enable-multilib
+make -j 16
+make install # This code will executed tomorrow
+
+echo "Installation of gcc is complete"
+echo "Now, we will link the libraries.."
+
+# just for darcy, because it is installed in the directory 12.3, not 12.3.0
+export gcc_ver=12.3
+export PATH=/home/ee331/gcc-$gcc_ver/bin:$PATH
+export LD_LIBRARY_PATH=$HOME/gcc-$gcc_ver/lib64:$LD_LIBRARY_PATH
+export CC=/home/ee331/gcc-$gcc_ver/bin/gcc
+export CXX=/home/ee331/gcc-$gcc_ver/bin/g++
+
+echo "The gcc installed in system now has a version of;"
+gcc -v
+gfortran -v
+
+
